5d90e453ffeadce13b0cdd0e35a642ed8785cde1
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-04-04 11:50:28 -0400

    1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const p = document.querySelector('p');
+    p.textContent = 'This is really cool!'
+  });
+  
